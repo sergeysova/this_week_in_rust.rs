2f884d4af87be4d13cdbd1f5c57b93b526d8e204
Sergey Sova <i.am@lestad.net>
2018-08-30 18:55:15 +0300

    fix: correctly parse last id

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,66 @@ extern crate reqwest;
 extern crate select;
 
 use select::document::Document;
-use select::predicate::{Class, Name, Predicate};
 
 use std::error::Error;
+use std::fs;
+use std::io::Write;
 
 mod parsers;
 mod types;
 
 use parsers::*;
 
+const SAVE_PATH: &'static str = "/tmp/this_week_in_rust_last_id.txt";
+
+fn read_last_id() -> Result<i32, Box<dyn Error + 'static>> {
+    let src = fs::read(SAVE_PATH)?;
+    let src = String::from_utf8_lossy(&src);
+    let value: i32 = src.trim().parse()?;
+
+    Ok(value)
+}
+
+// TODO: refactor to use with std::fs::OpenOptions
+fn save_last_id(id: i32) -> Result<(), Box<dyn Error + 'static>> {
+    if let Err(_) = fs::write(SAVE_PATH, id.to_string()) {
+        let mut file = fs::File::create(SAVE_PATH)?;
+        file.write_all(id.to_string().as_bytes())?;
+
+        Ok(())
+    } else {
+        Ok(())
+    }
+}
+
 fn run() -> Result<(), Box<dyn Error>> {
-    let last_id = 245;
+    let last_id = read_last_id().unwrap_or(0);
+    let mut last_id_to_be_saved = last_id;
 
+    println!("Now last ID is: {}", last_id);
+    println!("Start fetching list...");
     let html = reqwest::get("https://this-week-in-rust.org")?.text()?;
-
     let document = Document::from(html.as_str());
-
     let links = parse_home_page(&document, last_id);
 
-    for (ref id, link) in links {
-        println!("\n———\nFetching #{} — {}", id, link);
-        let res = parse_article(link, *id)?;
+    if links.len() == 0 {
+        println!("Nothing to send");
+    } else {
+        for (ref id, link) in links.iter().rev() {
+            println!("\n// ——— //\nFetching #{} — {}", id, link);
+            let res = parse_article(link, *id)?;
 
-        println!("{}", res);
+            println!("{}", res);
+
+            if *id > last_id_to_be_saved {
+                last_id_to_be_saved = *id;
+            }
+        }
+
+        save_last_id(last_id_to_be_saved)?;
     }
+
+    println!("Last ID now: {}", last_id_to_be_saved);
 
     Ok(())
 }
